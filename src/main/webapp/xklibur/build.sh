750264e29c149823aac92c4e3140c8beef30a53f
bitowl <bitowl@googlemail.com>
2013-06-14 14:59:53 +0200

    use a bash build script instead of ajax to include headers

---
+++ src/main/webapp/xklibur/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+log_msg() {
+    RED=$(tput setaf 1)
+    GREEN=$(tput setaf 2)
+    NORMAL=$(tput sgr0)
+    MSG="$1"
+    let COL=$(tput cols)-${#MSG}
+
+
+    printf "%${COL}s"  "$GREEN[DONE]$NORMAL"
+    echo ""
+}
+
+
+
+FILES=$(ls src/ | grep -v ^footer.html$ | grep -v ^header.html$ | grep -v ~$)
+for FILE in $FILES
+do
+echo -n "building $FILE..."
+cat src/header.html > $FILE
+cat src/$FILE >> $FILE
+cat src/footer.html >> $FILE
+log_msg "building $FILE..."
+done
