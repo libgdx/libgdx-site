949f5b7c4d9f5252a988634fb184e7cbf2c9a19e
Tom-Ski <tomaszwojciechowski@hotmail.co.uk>
2016-05-31 20:10:03 +0100

    Add author to Skins page

--- src/main/webapp/js/skins.js
+++ src/main/webapp/js/skins.js
@@ -15,9 +15,10 @@ function processSkins (data) {
     for (var i = 0; i < data.length; i++) {
 
         var title = "<h2>" + data[i].title + "</h2>";
+        var author = "<h3>by " + data[i].author + "</h3>";
         var link = "<a class='btn skin-download' href='libgdx-skins-dist/" + data[i].foldername + ".zip')><img src='img/save.png' height='28px'/>Download</a>";
 
-        var divString = "<div class='one-of-three' id=''>" + title + "<div class='skin-element' id='" + data[i].foldername + "-preview'></div>" + link + "</div>"
+        var divString = "<div class='one-of-three' id=''>" + title + author + "<div class='skin-element' id='" + data[i].foldername + "-preview'></div>" + link + "</div>"
 
         $('#skins').append(divString);
         $('#' + data[i].foldername + "-preview").css('background-image', 'url(libgdx-skins-dist/skin-previews/' + data[i].foldername + '-preview.png)');
