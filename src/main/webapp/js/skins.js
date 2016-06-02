2f8d19b8f689611b1f3a25ad5973a3d5e2c7d762
Michael Weinberger <michael.weinberger@lyze.at>
2016-06-02 22:31:33 +0200

    Reverted previous push and added a button the the file which instead opens the preview

--- src/main/webapp/js/skins.js
+++ src/main/webapp/js/skins.js
@@ -17,8 +17,9 @@ function processSkins (data) {
         var title = "<h2>" + data[i].title + "</h2>";
         var author = "<h3>by " + data[i].author + "</h3>";
         var link = "<a class='btn skin-download' href='libgdx-skins-dist/" + data[i].foldername + ".zip')><img src='img/save.png' height='28px'/>Download</a>";
+        var preview = "<a style='margin-right: 4px' class='btn skin-download' href='demos/libgdx-skins?skin=" + data[i].foldername + "')><img src='img/preview.png' style='margin-right:2px;' height='28px'/>Preview</a>";
 
-        var divString = "<div class='one-of-three' id=''>" + title + author + "<a href='demos/libgdx-skins?skin=" + data[i].foldername + "'><div class='skin-element' id='" + data[i].foldername + "-preview'></div></a>" + link + "</div>"
+        var divString = "<div class='one-of-three' id=''>" + title + author + "<div class='skin-element' id='" + data[i].foldername + "-preview'></div>" + preview + link + "</div>"
 
         $('#skins').append(divString);
         $('#' + data[i].foldername + "-preview").css('background-image', 'url(libgdx-skins-dist/skin-previews/' + data[i].foldername + '-preview.png)');
