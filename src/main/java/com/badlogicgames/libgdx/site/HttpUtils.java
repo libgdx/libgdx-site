6d5808430091b2e63d0780222faa170236fb0489
badlogic <contact@badlogicgames.com>
2018-05-05 20:56:06 +0200

    Fix all the things.

--- src/main/java/com/badlogicgames/libgdx/site/HttpUtils.java
+++ src/main/java/com/badlogicgames/libgdx/site/HttpUtils.java
@@ -38,7 +38,7 @@ public class HttpUtils {
 			return false;
 		}
 	}
-	
+
 	private static void write (File file, InputStream input) {
 		OutputStream output = null;
 		try {
@@ -62,12 +62,12 @@ public class HttpUtils {
 			}
 		}
 	}
-	
+
 	public interface Progress {
 		void setProgress(float progress);
 		float getProgress();
 	}
-	
+
 	public static int download (byte[] out, String url, Progress progress) {
 		HttpURLConnection conn = null;
 		try {
@@ -88,11 +88,11 @@ public class HttpUtils {
 			return 0;
 		}
 	}
-	
+
 	public static int download (byte[] out, String url) {
 		return download(out, url, null);
 	}
-	
+
 	private static int readBytes(byte[] out, InputStream in, int contentLength, Progress progress) {
 		try {
 			int readBytes = 0;
@@ -116,11 +116,11 @@ public class HttpUtils {
 			}
 		}
 	}
-	
+
 	public static int getHttp (char[] out, String url, String... parameters) {
 		return getHttp(out, url, null, parameters);
 	}
-	
+
 	/** Sends a GET request to the given URL
 	 * @param url
 	 * @return the response or null */
@@ -164,11 +164,11 @@ public class HttpUtils {
 			return 0;
 		}
 	}
-	
+
 	public static String getHttp (String url, String... parameters) {
 		return getHttp(url, null, parameters);
 	}
-	
+
 	/** Sends a GET request to the given URL
 	 * @param url
 	 * @return the response or null */
@@ -214,7 +214,7 @@ public class HttpUtils {
 			return null;
 		}
 	}
-	
+
 	/** URL-encodes the given parameters and posts them to the given url. The
 	 * content type is set to "application/x-www-form-urlencoded".
 	 * @param url
@@ -237,7 +237,7 @@ public class HttpUtils {
 	public static String postHttp (String url, String contentType, byte[] data) {
 		HttpURLConnection conn = null;
 		StringBuilder response;
-		
+
 		try {
 			URL commandURL = new URL(url);
 			conn = (HttpURLConnection)commandURL.openConnection();
@@ -280,8 +280,8 @@ public class HttpUtils {
 			return null;
 		}
 	}
-	
-	
+
+
 	public static HttpResponse postHttpWithCookies (String url, String[] cookies, String... parameters) {
 		String encodedParams = encodeParameters(parameters);
 		if(encodedParams == null) return null;
@@ -291,7 +291,7 @@ public class HttpUtils {
 			return null;
 		}
 	}
-	
+
 	/** Posts a UTF-8 encoded string to the given url, specifying the content type.
 	 * @param url
 	 * @param data
@@ -299,7 +299,7 @@ public class HttpUtils {
 	public static HttpResponse postHttpWithCookies (String url, String[] cookies, String contentType, byte[] data) {
 		HttpURLConnection conn = null;
 		StringBuilder response;
-		
+
 		try {
 			URL commandURL = new URL(url);
 			conn = (HttpURLConnection)commandURL.openConnection();
@@ -327,7 +327,7 @@ public class HttpUtils {
 			while ((ch = isr.read()) != -1) {
 				response.append((char)ch);
 			}
-			
+
 			HttpResponse resp = new HttpResponse();
 			for(String key: conn.getHeaderFields().keySet()) {
 				if(key == null) continue;
@@ -357,11 +357,11 @@ public class HttpUtils {
 			return null;
 		}
 	}
-	
+
 	public static HttpResponse getHttpWithCookies (String url, String[] cookies, String... parameters) {
 		return getHttpWithCookies(url, cookies, null, parameters);
 	}
-	
+
 	/** Sends a GET request to the given URL
 	 * @param url
 	 * @return the response or null */
@@ -369,9 +369,8 @@ public class HttpUtils {
 		HttpURLConnection conn = null;
 		String data = encodeParameters(parameters);
 		if(data == null) return null;
-		
+
 		try {
-			long start = System.nanoTime();
 			URL commandURL = new URL(url + (data.length() >0?"?" + data: ""));
 			conn = (HttpURLConnection)commandURL.openConnection();
 			conn.setDoInput(true);
@@ -419,14 +418,13 @@ public class HttpUtils {
 			return null;
 		}
 	}
-	
+
 	public static HttpResponse getHttpWithCookies (char[] out, String url, String[] cookies, Progress progress, String... parameters) {
 		HttpURLConnection conn = null;
 		String data = encodeParameters(parameters);
 		if(data == null) return null;
 
 		try {
-			long start = System.nanoTime();
 			URL commandURL = new URL(url + "?" + data);
 			conn = (HttpURLConnection)commandURL.openConnection();
 			conn.setDoInput(true);
@@ -472,7 +470,7 @@ public class HttpUtils {
 			return null;
 		}
 	}
-	
+
 	public static class HttpResponse {
 		public ArrayList<String> cookies = new ArrayList<String>();
 		public String response;
--- start.sh
+++ start.sh
@@ -33,5 +33,4 @@ cd src/main/webapp
 ./build.sh
 cd ../../..
 mvn clean package
-cp -r src/main/webapp/* /usr/share/nginx/html/
 java -jar target/libgdx-site-jar-with-dependencies.jar
