a021c14cd0e9642fa4ba2499202e1532fcd50d43
badlogic <contact@badlogicgames.com>
2018-05-07 03:43:17 +0200

    More flags.

--- start.sh
+++ start.sh
@@ -15,4 +15,4 @@ cd src/main/webapp
 ./build.sh
 cd ../../..
 mvn clean package
-java -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=8001 -Dcom.sun.management.jmxremote.rmi.port=8001 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=95.216.8.184 -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -jar target/libgdx-site-jar-with-dependencies.jar
+java -agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n -jar target/libgdx-site-jar-with-dependencies.jar
